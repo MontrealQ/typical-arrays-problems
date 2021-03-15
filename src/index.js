efae9e4f430d2f58429a7596ebae84f1cdc4bc5f
Artsiom Bandaryk <a.bondarik13@gmail.com>
2021-03-15 21:49:19 +0300

    init:  start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return Math.min.apply(null, array);
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return Math.max.apply(null, array);
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return (array.reduce((a, b) => a + b) / array.length);
+    }
+    return 0;
+};
